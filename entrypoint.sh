009d309a5f2d5ca5b391b4839a083bb173131d24
chaoqun <27287694+OpenWaygate@users.noreply.github.com>
2024-07-05 00:16:47 +0800

    :bug: Fix tag value

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ cd "$(dirname "$0")" || exit
 pipenv run python main.py
 file=$(ls "$GITHUB_WORKSPACE"/*.mp4)
 title=$(basename "$file" | cut -d'.' -f1)
-tag=$(basename "title" | cut -d'-' -f2)
+tag=$(echo "$title" | cut -d'-' -f2)
 echo "title=$title" >> "$GITHUB_OUTPUT"
 echo "tag=$tag" >> "$GITHUB_OUTPUT"
