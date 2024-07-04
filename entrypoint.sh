b77e0fcb67f32db28ebda78b376da102ec7848da
chaoqun <27287694+OpenWaygate@users.noreply.github.com>
2024-07-04 23:18:20 +0800

    :green_heart: Fix working path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 cd "$(dirname "$0")" || exit
 
 pipenv run python main.py
-file=$(ls /github/workspace/*.mp4)
+file=$(ls "$GITHUB_WORKSPACE"/*.mp4)
 title=$(basename "$file" | cut -d'.' -f1)
-tag=$(basename "$file" | cut -d'-' -f2)
+tag=$(basename "title" | cut -d'-' -f2)
 echo "title=$title" >> "$GITHUB_OUTPUT"
 echo "tag=$tag" >> "$GITHUB_OUTPUT"
