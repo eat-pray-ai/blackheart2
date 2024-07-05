512f7f3336f5b2f5a188f1849007ae1eefa08136
chaoqun <27287694+OpenWaygate@users.noreply.github.com>
2024-07-05 19:59:50 +0800

    :sparkles: Add video description and action cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,21 @@ cd "$(dirname "$0")" || exit
 pipenv run python main.py
 file=$(ls "$GITHUB_WORKSPACE"/*.mp4)
 title=$(basename "$file" | cut -d'.' -f1)
+word=$(echo "$title" | cut -d'-' -f1)
 tag=$(echo "$title" | cut -d'-' -f2)
-echo "title=$title" >> "$GITHUB_OUTPUT"
-echo "tag=$tag" >> "$GITHUB_OUTPUT"
+
+description=$(cat <<-EOM
+Keep learning with me 🌱
+${word} is a keyword in ${tag}, grasp it to make yourself one step further! 🤓
+Your subscription and thumb up👍 are my motivation to create more content 🤗
+
+The video's generation is AI🤖 Powered, wanna create your own video?
+Check out my Github project 🚀: https://github.com/eat-pray-ai/yutu
+EOM
+)
+
+{
+  echo "title=$title"
+  echo "tag=$tag"
+  echo "description=$description"
+} >> "$GITHUB_OUTPUT"
